d423f77cf69e6d44926eb55fd9bf6d72c0ffc342
rangapv <rangapv@yahoo.com>
2024-11-03 10:23:26 +0000

    seamless function-call

--- ver.sh
+++ ver.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 #author:rangapv@yahoo.com
 #02-11-24
-#To Check the package version currently installed versus the new version required
+
+#TO-run
+#vercheck new-version-number command-to-execute-to-get-current-version library-call
 
 vercheck() {
-#vrchk=("$@")
-#echo "vrchk is $vrchk"
-vrchk2="${vrchk[2]}"
-vrchk1="$@"
-vrchk2=`${vrchk1} -c "import zlib ; print(zlib.ZLIB_RUNTIME_VERSION)"`
-newver="1.3.1"
-#newver="${vrchk[1]}"
 
+vrchk=("$@")
+#echo "vrchk is $vrchk"
+vrchk21="${vrchk[1]}"
+newver="${vrchk[0]}"
+libadd="${vrchk[2]}"
+
+vrchk2=`eval "$vrchk21"`
 vrarg3=($(echo ${vrchk2} | awk '{len=split($0,a,"."); for (n=0;n<=len;n++) print a[n]}'))
 vrarg4=($(echo ${newver} | awk '{len=split($0,a,"."); for (n=0;n<=len;n++) print a[n]}'))
 lvrarg3=${#vrarg3[@]}
@@ -38,6 +40,7 @@ else
     then
        n=+1
     fi
+
     if [ "${vrarg4[n]}" -gt "${vrarg3[n]}" ]
     then
         echo "Upgrade required 1"
@@ -47,6 +50,7 @@ else
     echo "comparing ${vrarg4[n]} and ${vrarg3[n]}"
  done
  fi
+
  if [ "$lvrarg3" -lt "$lvrarg4" ]
  then
  for((n=0;n<$lvrarg4;++n))
@@ -83,29 +87,29 @@ else
 
  if [ $upgradeflag -eq 1 ]
  then
-  echo "Upgrading package"
-  #zlibadd
+         echo "The current version $vrchk2 is behind than the requested version $newver "
+         echo "Upgrading package"
+         #un-comment the following variable when ready to execute the library
+         #$libadd
  else
 	 echo "No upgrade required"
+	 echo "The current version $vrchk2 is newer than the requested version $newver "
  fi
 
 fi
 }
 
-
-
 print_echo() {
+echo "newver is $newver"
+echo "vrchk21 is $vrchk21"
+echo "libadd is $libadd"
 echo "lvrarg3 is $lvrarg3"
 echo "lvrarg4 is $lvrarg4"
 echo "vrchk1 is $vrchk2"
 echo "vrchk2 is $newver"
 echo "vrarg3 is ${vrarg3[@]}"
 echo "vrarg4 is ${vrarg4[@]}"
-
 }
 
-#clarg1="1.3.2"
-#clarg2="python3 -c "import zlib ; print(zlib.ZLIB_RUNTIME_VERSION)"
-#vercheck "\${clarg1}" "\${clarg2}"
-vercheck python3
+vercheck 1.4.0 "python3 -c \"import zlib;print(zlib.ZLIB_RUNTIME_VERSION)\"" zlibadd
 #print_echo
